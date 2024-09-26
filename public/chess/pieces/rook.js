6dd355358798a643dd5f51247a492676f448b008
DevMhrn <debashismaharana7854@gmail.com>
2024-09-26 15:30:31 +0530

    Implemented the rook piece
    Signed-off-by: DevMhrn <debashismaharana7854@gmail.com>

--- public/chess/pieces/rook.js
+++ public/chess/pieces/rook.js
@@ -1,11 +1,44 @@
-var Rook = function(config){
-    this.type = 'rook';
-    this.constructor(config);
+var Rook = function(config) {
+    config.type = 'rook';
+    Piece.call(this, config);
+ };
+ 
+ 
+ Rook.prototype = Object.create(Piece.prototype);
+ Rook.prototype.constructor = Rook;
+ 
+ 
+ Rook.prototype.isValidPosition = function(targetPosition) {
+    let currentCol = this.position.charAt(0);
+    let currentRow = parseInt(this.position.charAt(1));
+    let targetCol = targetPosition.col;
+    let targetRow = Number(targetPosition.row);
+ 
+ 
+    // Rooks move vertically or horizontally
+    if (targetCol === currentCol) { // Vertical movement
+        
+        const step = (targetRow > currentRow) ? 1 : -1;
+        for (let row = currentRow + step; row !== targetRow; row += step) {
+            if (this.board.getPieceAt({ col: currentCol, row: row })) {
+                return false; // Path is blocked
+            }
+        }
+        return true; // Valid rook move
+    }
+    else if (targetRow === currentRow) { // Horizontal movement
+        // Check if any pieces are blocking the path
+        const step = (targetCol > currentCol) ? 1 : -1;
+        for (let col = currentCol.charCodeAt(0) + step;
+             col !== targetCol.charCodeAt(0);
+             col += step) {
+            if (this.board.getPieceAt({ col: String.fromCharCode(col), row: currentRow })) {
+                return false; // Path is blocked
+            }
+        }
+        return true; // Valid rook move
+    }
+ 
+ 
+    return false; // Invalid move
 };
-
-
-
-Rook.prototype = new Piece({});
-Rook.prototype.move = function(newPosition){
-
-}
